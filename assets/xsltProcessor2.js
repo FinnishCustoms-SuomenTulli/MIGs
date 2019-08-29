88d3aaeb8ea874aebbe4e9989b3053cbc099f045
Mikael Gustafsson <mikael.gustafsson80@gmail.com>
2019-08-29 12:01:28 +0300

    Create xsltProcessor2.js

---
+++ assets/xsltProcessor2.js
@@ -0,0 +1,52 @@
+function loadXML(url) {
+    return new Promise(function(resolve) {
+        var req = new XMLHttpRequest();
+        req.open("GET", url);
+        if (typeof XSLTProcessor === 'undefined') {
+            try {
+                req.responseType = 'msxml-document';
+            } catch (e) {}
+        }
+        req.onload = function() {
+            resolve(this.responseXML)
+        }
+        req.send();
+    });
+}
+
+function transform(xmlUrl, xslUrl, targetElement, language, messageType) {
+    Promise.all([loadXML(xmlUrl), loadXML(xslUrl)]).then(function(data) {
+        var xmlDoc = data[0];
+        var xslDoc = data[1];
+
+        if (typeof XSLTProcessor !== 'undefined') {
+            var proc = new XSLTProcessor();
+            proc.importStylesheet(xslDoc);
+
+            proc.setParameter(null, "xmlUrl", xmlUrl);
+            proc.setParameter(null, "language", language);
+            proc.setParameter(null, "messageType", messageType);
+
+            var resultFrag = proc.transformToFragment(xmlDoc, targetElement.ownerDocument);
+
+            targetElement.textContent = '';
+            targetElement.appendChild(resultFrag);
+        } else {
+            var template = new ActiveXObject('Msxml2.XslTemplate.6.0');
+            template.stylesheet = xslDoc;
+            var proc = template.createProcessor();
+
+            proc.addParameter(null, "xmlUrl", xmlUrl);
+            proc.addParameter(null, "language", language);
+            proc.addParameter(null, "messageType", messageType);
+
+            proc.input = xmlDoc;
+
+            proc.transform();
+
+            var resultHTML = proc.output;
+
+            targetElement.innerHTML += resultHTML;
+        }
+    });
+ }
